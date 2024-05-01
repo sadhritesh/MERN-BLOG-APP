097a1b98d3dbf8f57049e5b16d35560f8d939c9d
Ritesh Sadh <sadhritesh32@gmail.com>
2024-05-01 14:34:19 +0530

    created pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Home, SignIn, SignUp, Projects, Dashboard, About, Error } from "./pages"
 
 function App() {
   
   return (
-    <h1 className="text-3xl font-bold underline text-red-600">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from "./Home.jsx";
+import Dashboard from "./Dashboard.jsx";
+import About from "./About.jsx";
+import Projects from "./Projects.jsx";
+import SignIn from "./SignIn.jsx";
+import SignUp from "./SignUp.jsx";
+import Error from "./Error.jsx";
+
+export {
+    Home,
+    Dashboard,
+    About,
+    Projects,
+    SignIn,
+    SignUp,
+    Error
+}
